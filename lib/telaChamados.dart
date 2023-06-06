8e44b57fe7f2120df030d8839aac663457b975c0
Fernando Souza <nandzsouza@gmail.com>
2023-06-06 17:22:45 -0300

    modificando tela Chamados

--- lib/telaChamados.dart
+++ lib/telaChamados.dart
@@ -78,7 +78,7 @@ class _TelaChamado1State extends State<TelaChamado1> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             SizedBox(height: 15),
-            Chamado_Analise(
+            CaixaAzul(
               logradouro: item['logradouro'],
               bairro: item['bairro'],
               complemento: item['complemento'],
@@ -132,95 +132,6 @@ class _TelaChamado1State extends State<TelaChamado1> {
         return getChamadoWidget(item);
       },
     );
-  }
-}
-
-class Chamado_Analise extends StatelessWidget {
-  final String logradouro;
-  final String bairro;
-  final String complemento;
-  final String dataAtualizacao;
-  final String descricao;
-
-  Chamado_Analise(
-      {required this.complemento,
-      required this.bairro,
-      required this.logradouro,
-      required this.dataAtualizacao,
-      required this.descricao});
-
-  //class Chamado_Analise extends StatelessWidget {
-  @override
-  Widget build(BuildContext buildContext) {
-    return Center(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(height: 20),
-        Stack(// Envolver o Container com um Stack
-            children: [
-          Container(
-            width: 350,
-            height: 150,
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 0, 4, 255),
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-          Positioned(
-            top: 10,
-            left: 50,
-            right: 10,
-            child: Container(
-              child:
-                  Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-                Text(
-                  logradouro,
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                Text(
-                  bairro,
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                Text(
-                  complemento,
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                //LINHA CINZA DEBAIXO DO TEXTO DA RUA
-                Container(
-                  height: 2,
-                  color: Color.fromARGB(255, 186, 186, 186),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                //DATA ABAIXO DA LINHA
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Status_Analise(),
-                    Text(
-                      dataAtualizacao,
-                      style: TextStyle(color: Colors.white, fontSize: 11),
-                    ),
-                  ],
-                ),
-              ]),
-            ),
-          ),
-          //ESPAÇAMENTO/POSICIONAMENTO DO CONTAINER AMARELO
-        ])
-      ],
-    ));
   }
 }
 
@@ -504,5 +415,241 @@ class Barra_do_app extends StatelessWidget {
         Icon(Icons.search, size: 40)
       ]),
     );
+  }
+}
+
+class CaixaAzul extends StatelessWidget {
+  final String logradouro;
+  final String bairro;
+  final String complemento;
+  final String dataAtualizacao;
+  final String descricao;
+
+  CaixaAzul({
+    required this.complemento,
+    required this.bairro,
+    required this.logradouro,
+    required this.dataAtualizacao,
+    required this.descricao,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 500,
+        height: 180,
+
+        //CONTAINER FLHO QUE É O AZUL
+        child: Stack(children: [
+          Positioned(
+              left: 23,
+              child: Container(
+                width: 350,
+                height: 180,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 38, 0, 255),
+                    borderRadius: BorderRadius.circular(20)),
+                //ESPAÇO DENTRO DO MOLDE AZUL QUE É ONDE FICA AS COISAS
+                child: Column(children: [
+                  //ENDEREÇO
+                  Container(
+                      width: 350,
+                      height: 90,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                              top: 15,
+                              right: 10,
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      complemento,
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20),
+                                    ),
+                                    Text(
+                                      bairro,
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20),
+                                    ),
+                                    Text(
+                                      logradouro,
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20),
+                                    )
+                                  ]))
+                        ],
+                      )),
+                  //LINHA CINZA
+                  Container(
+                      width: 350,
+                      height: 15,
+                      child: Stack(children: [
+                        Positioned(top: 10, right: 5, child: Linha())
+                      ])),
+                  //STATUS AMARELO E DATA EM UMA LINHA
+                  Container(
+                    width: 350,
+                    height: 75,
+                    //ROW COM O STATUS E A DATA
+                    child: Row(
+                      children: [
+                        //STATUS
+                        Container(
+                            width: 200,
+                            height: 90,
+                            child: Stack(children: [
+                              Positioned(
+                                  top: 25, left: -5, child: StatusChamado())
+                            ])),
+                        //DATA
+                        Container(
+                            width: 140,
+                            height: 40,
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                    top: 2,
+                                    right: 2,
+                                    child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.end,
+                                        children: [
+                                          Container(
+                                              width: 100,
+                                              height: 50,
+                                              child: Text(
+                                                dataAtualizacao,
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 16),
+                                                maxLines: 2,
+                                              ))
+                                        ]))
+                              ],
+                            ))
+                      ],
+                    ),
+                  ),
+                ]),
+              )
+
+              //ACABA O ESPAÇO DENTRO DO MOLDE
+              )
+        ]));
+  }
+}
+
+class Endereco extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      SizedBox(height: 5),
+      // ENDEREÇO DENTRO DO ALIGN
+      Align(
+          alignment: Alignment.topRight,
+          child: Column(children: [
+            //CONTAINER DO ENDEREÇO
+            Container(
+                width: 300,
+                child: Text(
+                  'Rua Manoel Bandeira,Bairro Jardim, Refência Supermercado Tonhão',
+                  maxLines: 3,
+                  textAlign: TextAlign.right,
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ))
+          ])),
+    ]);
+  }
+}
+
+class Linha extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 230,
+        height: 10,
+        child: Align(
+            alignment: Alignment.topRight,
+            child: Container(
+                width: 280,
+                child: Column(children: [
+                  Container(
+                    height: 2,
+                    decoration: BoxDecoration(color: Colors.grey),
+                  ),
+                ]))));
+  }
+}
+
+class Data extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 300,
+        height: 20,
+        child: Stack(
+          children: [
+            Positioned(
+                right: 5,
+                child: Container(
+                    width: 160,
+                    child: Stack(children: [
+                      Positioned(
+                          child: Column(children: [
+                        Container(
+                            width: 160,
+                            child: Text(
+                              '06/03/2023-20:17',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16),
+                              textAlign: TextAlign.right,
+                            ))
+                      ]))
+                    ])))
+          ],
+        ));
+  }
+}
+
+class StatusChamado extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        alignment: Alignment.topLeft,
+        child: Column(children: [
+          Container(
+              width: 300,
+              height: 50,
+              child: Stack(children: [
+                Positioned(
+                    left: 20,
+                    //STATUS AMARELO:
+                    child: Container(
+                        width: 180,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            borderRadius: BorderRadius.circular(30)),
+                        //POSICIONANDO O TEXTO DENTRO DO STATUS AMARELO COM UM COLUMN (EM ANÁLISE...)
+                        child: Column(children: [
+                          Container(
+                              width: 180,
+                              height: 40,
+                              child: Stack(children: [
+                                Positioned(
+                                    top: 7,
+                                    left: 10,
+                                    child: Text(
+                                      'EM ANÁLISE...',
+                                      style: TextStyle(
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.bold),
+                                    ))
+                              ]))
+                        ]) //STATUS AMARELO ACABA AQUI
+                        ))
+              ]))
+        ]));
   }
 }
